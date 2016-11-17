dd214b4ae291661ccdb5b46b711290346577ec2d
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2016-11-17 13:08:07 -0300

    Implementado algoritmo de contagem de tempo

--- classes/external.php
+++ classes/external.php
@@ -47,7 +47,6 @@ class local_monitor_external extends external_api
     public static function get_online_time($timeBetweenClicks, $startDate, $endDate, $tutor)
     {
         global $DB;
-        $DB->set_debug(false);
 
         self::validate_parameters(self::get_online_time_parameters(), array(
                 'time_between_clicks' => $timeBetweenClicks,
@@ -64,8 +63,9 @@ class local_monitor_external extends external_api
         $days = $interval / local_monitor_external::$day;
 
         $tutor = $DB->get_record('user', array('id' => $tutor));
+        $name  = $tutor->firstname . ' ' . $tutor->lastname;
 
-        for($i = $days; $i >= 1; $i--){
+        for($i = $days; $i != 1; $i--){
 
             $parameters = array(
                 (integer) $tutor,
@@ -81,13 +81,34 @@ class local_monitor_external extends external_api
                         AND timecreated <= ?
                         ORDER BY userid ASC";
 
+            $result = array();
+
             try {
+                $DB->set_debug(false);
                 $logs = $DB->get_records_sql($query, $parameters);
-                return var_dump($logs);
+                $date = gmdate("d-m-Y", $end - ( local_monitor_external::$day * $i ));
+
+                $previousLog     = array_shift($logs);
+                $previousLogTime = $previousLog->timecreated;
+                $sessionStart    = $previousLog->timecreated;
+                $onlineTime      = 0;
+
+                foreach ($logs as $log){
+                    if(($previousLogTime - $log->timecreated) < $timeBetweenClicks){
+                        $onlineTime  += $previousLogTime - $log->timecreated;
+                        $sessionStart = $log->timecreated;
+                    }
+
+                    $previousLogTime = $log->timecreated;
+                }
+
+                $result[$i] = (object) array('name' => $name, 'onlinetime' => $onlineTime, 'date' => $date);
             } catch (\Exception $e){
                 throw $e;
             }
         }
+
+        return print_r($result);
     }
 
     /**
@@ -97,7 +118,14 @@ class local_monitor_external extends external_api
      */
     public static function get_online_time_returns()
     {
-        // Todo: Implementar saida adequada com padrão REST
-        return new external_value(PARAM_TEXT, 'Retorna o tempo durante o qual o tutor esteve online');
+        return new external_multiple_structure(
+          new external_single_structure(
+              array(
+                  'name' => new external_value(PARAM_TEXT, 'Nome do Tutor'),
+                  'onlinetime' => new external_value(PARAM_INT, 'Tempo online'),
+                  'date' => new external_value(PARAM_TEXT, 'Data')
+              )
+          )
+        );
     }
 }
