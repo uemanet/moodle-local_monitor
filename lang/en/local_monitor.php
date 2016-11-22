70c60cf4ce5f43e1499cb32720c31fa87728e490
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2016-11-22 16:16:50 -0300

    Fix errors

--- lang/en/local_monitor.php
+++ lang/en/local_monitor.php
@@ -9,3 +9,8 @@
  */
 
 $string['pluginname'] = 'Monitor';
+
+// Errors strings
+$string['timebetweenclickserror'] = 'Time between clicks should be greater than 0';
+$string['startdateerror'] = 'Start date should be less than end date';
+$string['enddateerror'] = 'End date should be equals to or less than today date';
