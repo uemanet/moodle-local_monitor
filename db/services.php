76a4102fe75f7ac51c94b1928af3861b77896b1f
PedroFellipe <pedrofellipemelo123@gmail.com>
2017-10-24 16:27:48 -0300

    endpoint para análise de tutores nos fóruns criado

--- db/services.php
+++ db/services.php
@@ -15,20 +15,25 @@ $functions = array(
         'description' => 'Tutor online time',
         'type' => 'read'
     ),
-
     'monitor_ping' => array(
         'classname' => 'local_monitor_ping',
         'methodname' => 'monitor_ping',
         'classpath' => 'local/monitor/classes/ping',
         'description' => 'Checks the connection with Moodle',
         'type' => 'read'
+    ),
+    'monitor_tutor_answers' => array(
+        'classname' => 'local_monitor_forum',
+        'methodname' => 'get_tutor_forum_answers',
+        'classpath' => 'local/monitor/classes/forum',
+        'description' => 'Get tutor answers to foruns',
+        'type' => 'read'
     )
 );
 
-
 $services = array(
     'Monitor' => array(
-        'functions' => array('get_tutor_online_time', 'monitor_ping'),
+        'functions' => array('get_tutor_online_time', 'monitor_ping', 'monitor_tutor_answers'),
         'restrictedusers' => 0,
         'enabled' => 1
     )
